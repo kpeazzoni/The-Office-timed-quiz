fb385a6ae86a0481525b984f94f842e65e1555fe
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-01-05 09:46:55 -0500

    added questions in js, started a for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,115 @@
+// window.onload = function () {
 // create a buttom- attribure "onclick = function created in js. in my fuction create console.log("button was clicked") to test code
 // when start button is clicked, it needs to reveal questions
-var startButton = document.getElementById("#start-button");
-var click = "hidden"
+var startButton = document.getElementById("start-button");
 
-function startQuiz() {
-    if (click === "hidden") {
-        click === "reveal"
-    }
-console.log("button was clicked");
+    startButton.addEventListener("click", function() {
+    document.getElementById("start-quiz").style.visibility = "hidden";
+
+    var questionsDiv = document.getElementById("questions-div");
+    var questionWrapper = document.getElementById("questionWrapper");
+
+     // create a h2 element 
+    // create question text in h2 element 
+    questionWrapper.removeChild(questionsDiv);
+
+    var questionText = document.createElement("h2");
+    questionText.textContent = allQuestions[0].question;
+    myDiv.appendChild(questionText);
+      
+    // create a for loop through the answers (a, b, c) that creates checkbox and label element for each
+    // creating checkbox element
+    var answers = allQuestions[0].answer;
+
+    for (const property in answers) {
+        console.log(`${property}: ${answers[property]}`);
+     
+      
+    var checkbox = document.createElement('input');
+        
+    // Assigning the attributes
+    // to created checkbox
+    checkbox.type = "checkbox";
+    checkbox.value = property; //if statement to verify if answer is correct- if checkbox.value = correctAnswer, alert- correct
+    checkbox.id = "answer-" + property;
+   
+         
+    // creating label for checkbox
+    var label = document.createElement('label');
+        
+    // assigning attributes for
+    // the created label tag
+    label.htmlFor = "answer-" + property;
+        
+    // appending the created text to
+    // the created label tag
+    label.appendChild(document.createTextNode(answers[property]));
+        
+    // appending the checkbox
+    // and label to div
+    myDiv.appendChild(checkbox);
+    myDiv.appendChild(label);
 }
+});
+
 // User is presented with 5 questions - array of objects
     //  Format for questions???
-        // Object that conatins {question: string, Choices: array, answer: string}
+    // Object that conatins {question: string, Choices: array, answer: string}
+    var allQuestions = [
+        {
+            question: "What did Michael pretend to fire Pam over in Season 1?",
+            answer:  {
+                a: "Stealing Post-it Notes",
+                b: "Not answering the phone",
+                c: "Wearing her glasses"
+            },
+            questionId: 1,
+            correctAnswer: "a"
+        },
+        {
+            question: "What does Kevin suggest Dwight put in his gun holster?",
+            answer: {
+                a: "A cell phone",
+                b: "A banana",
+                c: "A toy gun"
+            },
+            questionId: 2,
+            correctAnswer: "b"
+        },
+        {
+            question: "What did Kevin buy for himself when he got himself for Secret Santa?",
+            answer: {
+                a: "A foot massager",
+                b: "A foot bath",
+                c: "M&M's"
+            },
+            questionId: 3,
+            correctAnswer: "b"
+        },
+        {
+            question: "What's Stanley's morning 3 by 5?",
+            answer: {
+                a: "Hot tea, 3 sugars, 5 creams",
+                b: "Iced tea, 3 sugars, 5 creams",
+                c: "Coffee, 3 sugars, 5 creams"
+            },
+            questionId: 4,
+            correctAnswer: "b"
+        },
+        {
+            question: "What radio station stickers are seen around the office?",
+            answer: {
+                a: "Froggy 901",
+                b: "Dr. Ira Glass",
+                c: "Froggy 101"
+            },
+            questionId: 5,
+            correctAnswer: "c"
+        }
+    ];
+
+    
+   
         // Display question - loop
         //  append question
         // append choices array
@@ -24,6 +122,8 @@ console.log("button was clicked");
 
     //  When all questions are answered display form to submit initials
     //  save form values score and initials to local STrorage
+    var endScore = localStorage.setItem()
+    var playerInitials = localStorage.setItem()
 
 //  change to highscores HTML
     //  read values from localstorage
