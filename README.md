49d8eeb3cee29e72573d805326bdcf8cebab008f
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-01-05 20:07:07 -0500

    added functions

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # UTA-bootcamp-timed-quiz
 timed quiz for challenge 4
+
+# Module4-Challenge Timed Quiz
+
+## Description:
+This Challenge invites you to build a timed coding quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface. 
+
+This week’s coursework will equip you with all the skills you need to succeed in this assignment.
+
+
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Credits](#credits) - collaboration with study group
+- [License](#license)
+- [Mock-up](#mock-up)
+
+## Usage
+- Github page https://github.com/kpeazzoni/UTA-bootcamp-timed-quiz
+- URL https://kpeazzoni.github.io/UTA-bootcamp-timed-quiz/
+
+![Horiseon-webpage](./assets/portfolio-module2.png)
+
+## Credits
+- As part of bootcamp module 2 challenged I have referenced sites shown below including mini-challenge 2 within UT-Austin bootcamp program:
+https://developer.mozilla.org/en-US/docs/Web/CSS/grid-template-columns
+https://developer.mozilla.org/en-US/docs/Web/CSS/grid-template-columns
+https://css-tricks.com/almanac/properties/g/grid-template-columns/
+## License
+
+Please refer to the LICENSE in the repo.
+
+
+
+## Mock-up##
+https://github.com/kpeazzoni/UTA-bootcamp-timed-quiz/tree/main/assets/images    
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,184 @@
 // window.onload = function () {
 // create a buttom- attribure "onclick = function created in js. in my fuction create console.log("button was clicked") to test code
 // when start button is clicked, it needs to reveal questions
+var highScores = 0;//end timer count
+var questionsDiv = document.getElementById("questions-div");
+var questionWrapper = document.getElementById("questionWrapper");
+var questionIndex = 0;
 var startButton = document.getElementById("start-button");
 
-    startButton.addEventListener("click", function() {
-    document.getElementById("start-quiz").style.visibility = "hidden";
+startButton.addEventListener("click", function() {
+document.getElementById("start-quiz").style.display = "none";
+var timerCountdown = document.querySelector(".timer");
+var secondsLeft = 60;
+timerCountdown.textContent = secondsLeft
+generateQuestions();
+setTime();
+});
 
-    var questionsDiv = document.getElementById("questions-div");
-    var questionWrapper = document.getElementById("questionWrapper");
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerCountdown.textContent = "Timer:" + secondsLeft
+        if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+                    // endGame(); //need to write this function
+    };
+    }, 1000);
+};
 
-     // create a h2 element 
-    // create question text in h2 element 
-    // questionWrapper.removeChild(questionsDiv);
 
+function generateQuestions() {
     var questionText = document.createElement("h2");
-    questionText.textContent = allQuestions[0].question;
+    questionsDiv.innerHTML= "";
+    questionText.textContent = allQuestions[questionIndex].question;
     questionsDiv.appendChild(questionText);
-      
+    document.getElementById("answer").innerHTML= "";
+    for (var i=0; i < allQuestions[questionIndex].answer.length; i++) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.textContent=allQuestions[questionIndex].answer[i];
+        choiceBtn.setAttribute("value", allQuestions[questionIndex].answer[i]);
+        choiceBtn.onclick = checkAnswers;
+        document.getElementById("answer").append(choiceBtn);
+    }
+
+    console.log(allQuestions[0].question);
+    console.log(allQuestions[0].answer);
+};
+
+function checkAnswers() {
+if (this.value === allQuestions[questionIndex].correctAnswer) {
+    document.getElementById("answer-message").textContent = "Correct! 5 seconds added to timer!";
+    secondsLeft += 5;
+    timerCountdown.textContent = "Timer:" + secondsLeft
+} else {
+    document.getElementById("answer-message").textContent = "Incorrect, minus 5 seconds from timer!";
+    secondsLeft -= 5;
+    timerCountdown.textContent = "Timer:" + secondsLeft
+    if (secondsLeft <= 0) {
+        secondsLeft = 0
+        //endGame();
+    }
+} 
+    questionIndex++ 
+    if (questionIndex === allQuestions.length) {
+        //endGame();
+    } else {
+        generateQuestions();
+    }
+};
+
+function endGame() {
+    questionWrapper.style.display = "none";
+    document.getElementById("final-score").classList.remove("hidden");
+    document.getElementById("score").textContent = secondsLeft;
+    document.getElementById("submit-btn").onclick = saveScore // write this function
+};
+// savescore is going to grab the value of input and get to local storage
     // create a for loop through the answers (a, b, c) that creates checkbox and label element for each
     // creating checkbox element
-    var answers = allQuestions[0].answer;
+    // var answers = allQuestions[0].answer;
 
-    for (const property in answers) {
-        console.log(`${property}: ${answers[property]}`);
+    // // for (const property in answers) {
+    // //     console.log(`${property}: ${answers[property]}`);
+
+    // for (var i = 0; i < answer.length; i++) {
+
+    // }
      
       
-    var checkbox = document.createElement('input');
+// var checkbox = document.createElement('input');
+    
+// // Assigning the attributes
+// // to created checkbox
+// checkbox.type = "checkbox";
+// checkbox.value = allQuestions.answer;
+// // checkbox.value = property; //if statement to verify if answer is correct- if checkbox.value = correctAnswer, alert- correct
+// checkbox.id = "answer-" + property;
+
         
-    // Assigning the attributes
-    // to created checkbox
-    checkbox.type = "checkbox";
-    checkbox.value = property; //if statement to verify if answer is correct- if checkbox.value = correctAnswer, alert- correct
-    checkbox.id = "answer-" + property;
-   
-         
-    // creating label for checkbox
-    var label = document.createElement('label');
-        
-    // assigning attributes for
-    // the created label tag
-    label.htmlFor = "answer-" + property;
-        
-    // appending the created text to
-    // the created label tag
-    label.appendChild(document.createTextNode(answers[property]));
-        
-    // appending the checkbox
-    // and label to div
-    questionsDiv.appendChild(checkbox);
-    questionsDiv.appendChild(label);
-}
-});
+// // creating label for checkbox
+// var label = document.createElement('label');
+    
+// // assigning attributes for
+// // the created label tag
+// label.htmlFor = "answer-" + property; //should this now be answer[]
+    
+// // appending the created text to
+// // the created label tag
+// label.appendChild(document.createTextNode(answers[property]));
+    
+// // appending the checkbox
+// // and label to div
+// questionsDiv.appendChild(checkbox);
+// questionsDiv.appendChild(label);
+
+// to display a message if the answer is correct or incorrect
+
+
+
+
+// could this be one way of hiding / revealing my questions?
+                    // var ul = document.parent("questions-div").createElement('ul');
+                    //         document.body.appendChild(ul);
+
+                    // var li1 = document.createElement('li');
+                    // var li2 = document.createElement('li');
+                    // ul.appendChild(li1);
+                    // ul.appendChild(li2);
+
+                    // function hide(evt) {
+                    //   // evt.target refers to the clicked <li> element
+                    //   // This is different than evt.currentTarget, which would refer to the parent <ul> in this context
+                    //   evt.target.style.visibility = 'hidden';
+                    // }
+
+                    // Attach the listener to the list
+                    // It will fire when each <li> is clicked
+                    // ul.addEventListener('click', hide, false);
+
+
 
 // User is presented with 5 questions - array of objects
     //  Format for questions???
     // Object that conatins {question: string, Choices: array, answer: string}
-    var allQuestions = [
-        {
-            question: "What did Michael pretend to fire Pam over in Season 1?",
-            answer:  {
-                a: "Stealing Post-it Notes",
-                b: "Not answering the phone",
-                c: "Wearing her glasses"
-            },
-            questionId: 1,
-            correctAnswer: "a"
+var allQuestions = [
+    {
+        question: "What did Michael pretend to fire Pam over in Season 1?",
+        answer: ["Stealing Post-it Notes", "Not answering the phone", "Wearing her glasses"],
+        questionId: 1,
+        correctAnswer: "Stealing Post-it Notes"    
+    },
+        
+    {
+        question: "What does Kevin suggest Dwight put in his gun holster?",
+        answer:["A cell phone", "A banana", "A toy gun"],
+        questionId: 2,
+        correctAnswer: "A banana"
         },
-        {
-            question: "What does Kevin suggest Dwight put in his gun holster?",
-            answer: {
-                a: "A cell phone",
-                b: "A banana",
-                c: "A toy gun"
-            },
-            questionId: 2,
-            correctAnswer: "b"
+        
+    {
+        question: "What did Kevin buy for himself when he got himself for Secret Santa?",
+        answer: ["A foot massager", "A foot bath", "M&M's"],
+        questionId: 3,
+        correctAnswer: "b"
         },
-        {
-            question: "What did Kevin buy for himself when he got himself for Secret Santa?",
-            answer: {
-                a: "A foot massager",
-                b: "A foot bath",
-                c: "M&M's"
-            },
-            questionId: 3,
-            correctAnswer: "b"
+        
+    {
+        question: "What's Stanley's morning 3 by 5?",
+        answer: ["Hot tea, 3 sugars, 5 creams", "Iced tea, 3 sugars, 5 creams", "Coffee, 3 sugars, 5 creams"],
+        questionId: 4,
+        correctAnswer: "Iced tea, 3 sugars, 5 creams"
         },
-        {
-            question: "What's Stanley's morning 3 by 5?",
-            answer: {
-                a: "Hot tea, 3 sugars, 5 creams",
-                b: "Iced tea, 3 sugars, 5 creams",
-                c: "Coffee, 3 sugars, 5 creams"
-            },
-            questionId: 4,
-            correctAnswer: "b"
+        
+    {
+        question: "What radio station stickers are seen around the office?",
+        answer: ["Froggy 901", "Dr. Ira Glass", "Froggy 101"],
+        questionId: 5,
+        correctAnswer: "Froggy 101"
         },
-        {
-            question: "What radio station stickers are seen around the office?",
-            answer: {
-                a: "Froggy 901",
-                b: "Dr. Ira Glass",
-                c: "Froggy 101"
-            },
-            questionId: 5,
-            correctAnswer: "c"
-        }
-    ];
+];
 
     
    
@@ -117,8 +189,8 @@ var startButton = document.getElementById("start-button");
 // user selects an answer(button,radio,checkboxes)data-answer = ""
     //  click event is on the parent container
     //  how to know which element was clicked(event.target)
-    // if the answer is correct display next quesrion access array of questions object 
-    // if answer is incorrect add 15 seconds to score and display next question
+    // if the answer is correct display next question access array of questions object 
+    // if answer is incorrect subtract 10 seconds to score and display next question
 
     //  When all questions are answered display form to submit initials
     //  save form values score and initials to local STrorage
@@ -128,20 +200,3 @@ var startButton = document.getElementById("start-button");
 //  change to highscores HTML
     //  read values from localstorage
     //  append score values to page
-
-// var timerCountdown = document.querySelector(".timer");
-// var secondsLeft = 5;
-
-// set timer to count down from 50 to 0. When it's at 0, display message "you lose"
-// function setTime() {
-// var timerInterval = setInterval(function() {
-//     secondsLeft--;
-//     timerCountdown.textContent = "Timer:" + secondsLeft
-//     if(secondsLeft === 0) {
-//         // Stops execution of action at set interval
-//         alert("You Lose!");
-//         // clearInterval(timerInterval);
-// };
-// }, 1000);
-// };
-// setTime();
